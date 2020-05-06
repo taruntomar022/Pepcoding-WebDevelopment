eff6ce13c3922009c6997f26d2961c986aa332b3
Tarun Tomar <taruntomar022@icloud.com>
2020-05-06 22:03:20 +0530

    CreateChallenge hackerarnk activity using selenium

--- Lecture 8/createChallenge.js
+++ Lecture 8/createChallenge.js
@@ -1,7 +1,7 @@
 let fs = require("fs");
 require("chromedriver");
 let swd = require("selenium-webdriver");
-let bldr = new swd.Builder();
+let bldr = new swd.Builder(); 
 let driver = bldr.forBrowser("chrome").build();
 
 let cFile = process.argv[2];
